f9972c8d74e956d966f1022855f0d4032b7d92b0
Kentoy <jankenith.johannessen@gmail.com>
2017-07-03 17:43:35 +0800

    updated process env

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get("/webhook", function (req, res) {
-  if (req.query["hub.verify_token"] === "this_is_my_token") {
+  if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
     console.log("Verified webhook");
     res.status(200).send(req.query["hub.challenge"]);
   } else {
