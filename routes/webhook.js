3612be1cbec2b8b0f0738db5fc6e518eb86b4421
Kentoy <jankenith.johannessen@gmail.com>
2017-07-03 20:40:27 +0800

    Added webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -38,25 +38,27 @@ function processPostback(event) {
   if (payload === "Greeting") {
     // Get user's first name from the User Profile API
     // and include it in the greeting
-    request({
-      url: "https://graph.facebook.com/v2.6/" + senderId,
-      qs: {
-        access_token: process.env.PAGE_ACCESS_TOKEN,
-        fields: "first_name"
-      },
-      method: "GET"
-    }, function(error, response, body) {
-      var greeting = "";
-      if (error) {
-        console.log("Error getting user's name: " +  error);
-      } else {
-        var bodyObj = JSON.parse(body);
-        name = bodyObj.first_name;
-        greeting = "Hi " + name + ". ";
-      }
-      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
-      sendMessage(senderId, {text: message});
-    });
+    // request({
+    //   url: "https://graph.facebook.com/v2.6/" + senderId,
+    //   qs: {
+    //     access_token: process.env.PAGE_ACCESS_TOKEN,
+    //     fields: "first_name"
+    //   },
+    //   method: "GET"
+    // }, function(error, response, body) {
+    //   var greeting = "";
+    //   if (error) {
+    //     console.log("Error getting user's name: " +  error);
+    //   } else {
+    //     var bodyObj = JSON.parse(body);
+    //     name = bodyObj.first_name;
+    //     greeting = "Hi " + name + ". ";
+    //   }
+    //   var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+    //   sendMessage(senderId, {text: message});
+    // });
+    var message = "Hi! My name is Shopcast. I can tell you various details regarding promos and events. Would you like to know about any events happening near you?";
+    sendMessage(senderId, {text: message});
   }
 }
 
