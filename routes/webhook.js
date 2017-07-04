a183caa6689216c357e2b8405fc16e65c316af68
Kentoy <jankenith.johannessen@gmail.com>
2017-07-04 12:09:42 +0800

    .

--- routes/webhook.js
+++ routes/webhook.js
@@ -161,7 +161,7 @@ function sendGenericMessage(recipientId) {
 
 function callSendAPI(messageData) {
   console.log("callSendAPI: ", messageData);
-  express.request({
+  request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
     qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
     method: 'POST',
