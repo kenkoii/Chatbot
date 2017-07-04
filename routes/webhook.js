2ce142ddc80bd2e205a332882e7237a3badff964
Kentoy <jankenith.johannessen@gmail.com>
2017-07-04 11:08:18 +0800

    reverted to quickstart

--- routes/webhook.js
+++ routes/webhook.js
@@ -52,6 +52,7 @@ function receivedMessage(event) {
   var timeOfMessage = event.timestamp;
   var message = event.message;
 
+  console.log(JSON.stringify(event));
   console.log("Received message for user %d and page %d at %d with message:", 
     senderID, recipientID, timeOfMessage);
   console.log(JSON.stringify(message));
