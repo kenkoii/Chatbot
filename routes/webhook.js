532c3eb6972bb54c9ef54831fcf0645acd1d32af
Kentoy <jankenith.johannessen@gmail.com>
2017-07-04 11:33:50 +0800

    breakpoints

--- routes/webhook.js
+++ routes/webhook.js
@@ -89,7 +89,7 @@ function sendTextMessage(recipientId, messageText) {
       text: messageText
     }
   };
-
+  console.log("sendTextMessage: ", messageData);
   callSendAPI(messageData);
 }
 
@@ -160,12 +160,16 @@ function sendGenericMessage(recipientId) {
 }
 
 function callSendAPI(messageData) {
+  console.log("callSendAPI: ", messageData);
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
     qs: { access_token: PAGE_ACCESS_TOKEN },
     method: 'POST',
     json: messageData
   }, function (error, response, body) {
+    console.log("response: ", response);
+    console.log("body: ", body);
+    console.log("error: ", error);
     if (!error && response.statusCode == 200) {
       var recipientId = body.recipient_id;
       var messageId = body.message_id;
