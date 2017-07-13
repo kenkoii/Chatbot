27945531875a7a7d1638c6237b50ca3ba410cf5a
Kentoy <jankenith.johannessen@gmail.com>
2017-07-13 10:47:06 +0800

    revert

--- routes/webhook.js
+++ routes/webhook.js
@@ -108,7 +108,7 @@ function receivedMessageEvent(event) {
     // and send back the example. Otherwise, just echo the text we received.
     switch (messageText) {
       case 'generic':
-        sendLocationButton(senderID);
+        sendGenericMessage(senderID);
         break;
       default:
         console.log("Default message");
@@ -242,8 +242,6 @@ function receivedPostbackEvent(event) {
   
 }
 
-
-
 function sendGenericMessage(recipientId) {
   var messageData = {
     recipient: {
@@ -288,6 +286,137 @@ function sendGenericMessage(recipientId) {
     }
   };  
   callSendAPI(messageData);
+}
+
+function sendPromoMessage(recipientId, coordinates) {
+  const lat = coordinates.lat;
+  const long = coordinates.long
+  request({
+    uri: 'https://api.shopcast.ph/v2/posts',
+    qs: { group_offers: true,
+          lat: lat,
+          lng: long,
+          merchant_photo: true,
+          page: 1,
+          pagesize: 100,
+          radius: 99
+         },
+    method: 'GET'
+  }, function (error, response, body) {
+    console.log("response: ", response);
+    console.log("body: ", body);
+    console.log("error: ", error);
+    const posts = JSON.parse(body);
+    const elements = [];
+    for(var i = 0; i < 4; i++) {
+      elements.push({
+        title: posts[i].title,
+        subtitle: posts[i].summary,
+        item_url: "http://shopcast.ph/map/post/" + posts[i].offer_id,           
+        image_url: "https://s3-ap-southeast-1.amazonaws.com/shopcastbucket/post/photo/" + posts[i].photo_file,
+        buttons: [{
+          type: "web_url",
+          url: "https://www.oculus.com/en-us/rift/",
+          title: "Open Web URL"
+        }]
+      });
+    }
+
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "list",
+            elements: elements
+          }
+        }
+      }
+    };  
+
+    callSendAPI(messageData);
+  }); 
+}
+
+function sendEventsMessage(recipientId, coordinates) {
+  const lat = coordinates.lat;
+  const long = coordinates.long
+  request({
+    uri: 'https://api.shopcast.ph/v2/events',
+    qs: { group_offers: true,
+          lat: lat,
+          lng: long,
+          merchant_photo: true,
+          page: 1,
+          pagesize: 100,
+          radius: 99
+         },
+    method: 'GET'
+  }, function (error, response, body) {
+    console.log("response: ", response);
+    // console.log("body: ", body);
+    console.log("error: ", error);
+    const posts = JSON.parse(body);
+    const elements = [];
+    for(var i = 0; i < 4; i++) {
+      elements.push({
+        title: posts[i].title,
+        subtitle: posts[i].summary,
+        item_url: "http://shopcast.ph/map/post/" + posts[i].offer_id,           
+        image_url: "https://s3-ap-southeast-1.amazonaws.com/shopcastbucket/post/photo/" + posts[i].photo_file,
+        buttons: [{
+          type: "web_url",
+          url: "https://www.oculus.com/en-us/rift/",
+          title: "Open Web URL"
+        }]
+      });
+    }
+
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "list",
+            elements: elements
+          }
+        }
+      }
+    };  
+
+    callSendAPI(messageData);
+  }); 
+}
+
+function callSendAPI(messageData) {
+  console.log("callSendAPI: ", messageData);
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+  }, function (error, response, body) {
+    console.log("response: ", response);
+    console.log("body: ", body);
+    console.log("error: ", error);
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
 }
 
 module.exports = router;
